d74b3413adc99a0c4529def4b4c336e82b9ff741
Agostnho F  Teles Jr. <atelesjr@gmail.com>
2026-03-14 18:45:16 -0300

    feat: update OrdersGrid component to enhance table headers and structure

--- components/OrderGrid/index.tsx
+++ components/OrderGrid/index.tsx
@@ -13,23 +13,27 @@ const OrdersGrid = ({ orders }: OrdersGridProps) => {
 				<table className="orders-grid__table">
 					<thead>
 						<tr className="orders-grid__head-row">
-							<th className="orders-grid__th">Ativo</th>
+							<th className="orders-grid__th">Instrumento</th>
 							<th className="orders-grid__th">Lado</th>
 							<th className="orders-grid__th">Preço</th>
 							<th className="orders-grid__th">Qtd</th>
+							<th className="orders-grid__th">Qtd Restante</th>
 							<th className="orders-grid__th">Status</th>
+							<th className="orders-grid__th">Data/Hora</th>
 						</tr>
 					</thead>
 					<tbody>
-					{orders.map((order) => (
+						{orders.map((order) => (
 							<tr className="orders-grid__row" key={order.id}>
 								<td className="orders-grid__td">{order.instrument}</td>
 								<td className="orders-grid__td">{order.side}</td>
 								<td className="orders-grid__td">{order.price}</td>
 								<td className="orders-grid__td">{order.quantity}</td>
+								<td className="orders-grid__td">{order.remainingQuantity}</td>
 								<td className="orders-grid__td">{order.status}</td>
-						</tr>
-					))}
+								<td className="orders-grid__td">{order.timestamp}</td>
+							</tr>
+						))}
 					</tbody>
 				</table>
 			</div>
